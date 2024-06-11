28b23fd5f7cf630c434d6b7f4fbd808af3a9a1f8
ucarrr <ucaaar07@gmail.com>
2024-06-12 01:48:50 +0300

    profile screen edited

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,31 +1,35 @@
-import { StyleSheet, Text, View, StatusBar, Alert } from 'react-native';
-import React, { useState, useEffect } from 'react';
-import { Title, Avatar, Caption, TouchableRipple } from 'react-native-paper';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, Text, View, Alert, Linking, ScrollView, Dimensions} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {TouchableRipple} from 'react-native-paper';
 import Icon1 from 'react-native-vector-icons/AntDesign';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import { AnimatedCircularProgress } from 'react-native-circular-progress';
-import { Dimensions } from 'react-native';
-import { useFocusEffect } from '@react-navigation/native';
+import {AnimatedCircularProgress} from 'react-native-circular-progress';
+import {useFocusEffect} from '@react-navigation/native';
 import axios from 'axios';
-import { URLs } from '../services/index';
+import {URLs} from '../services/index';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
-export default function Profile({ navigation }) {
-  const [state, setState] = React.useState({ open: false });
+export default function Profile({navigation}) {
   const [userInfo, setUserInfo] = useState(null);
   const [progress, setProgress] = useState(0);
+  const [links, setLinks] = useState([]);
+  const [completedSteps, setCompletedSteps] = useState(0);
+  const [remainingSteps, setRemainingSteps] = useState(0);
 
   useFocusEffect(
     React.useCallback(() => {
       fetchUserProfile();
-    }, [])
+      fetchLinks();
+    }, []),
   );
 
   useEffect(() => {
     if (userInfo) {
+      console.log('userInfo', JSON.stringify(userInfo, null, 2));
+      calculateSteps(userInfo.steps);
       setProgress(userInfo.progress);
     }
   }, [userInfo]);
@@ -37,12 +41,9 @@ export default function Profile({ navigation }) {
       const userUrl = `${URLs.BASE_URL}users/${userData._id}`;
       const userResponse = await axios.get(userUrl);
       const user = userResponse.data;
-      
-      console.log('User Profile:', userData._id);
-      
-      setProgress(user.progress);
-      setUserInfo(user);
 
+      console.log('Fetched user data:', JSON.stringify(user, null, 2));
+      setUserInfo(user);
       return user;
     } catch (error) {
       console.error('Error fetching user profile:', error);
@@ -50,91 +51,123 @@ export default function Profile({ navigation }) {
     }
   };
 
+  const fetchLinks = async () => {
+    try {
+      console.log(`${URLs.BASE_URL}link`);
+      const linksResponse = await axios.get(`${URLs.BASE_URL}link`);
+
+      console.log('linksResponse', JSON.stringify(linksResponse.data, null, 2));
+      setLinks(linksResponse.data);
+    } catch (error) {
+      console.error('Error fetching links:', error);
+      Alert.alert('Error', 'Failed to load links');
+    }
+  };
+
+  const calculateSteps = steps => {
+    if (!steps) {
+      console.log('Steps not defined');
+      return;
+    }
+
+    let completed = 0;
+    let remaining = 0;
+
+    steps.forEach(step => {
+      step.stepDetails.forEach(detail => {
+        if (detail.isCompleted) {
+          completed += 1;
+        } else {
+          remaining += 1;
+        }
+      });
+    });
+
+    setCompletedSteps(completed);
+    setRemainingSteps(remaining);
+    console.log(`Completed steps: ${completed}, Remaining steps: ${remaining}`);
+  };
+
+  useEffect(() => {
+    console.log('Links state:', links);
+  }, [links]);
+
   if (!userInfo) {
-    return <Text>Loading...</Text>;
-  } 
-
-  const onStateChange = ({ open }) => setState({ open });
-
-  const { open } = state;
+    return <Text style={styles.loading}>Loading...</Text>;
+  }
 
   const logout = async () => {
     await AsyncStorage.clear();
-    console.log("Logout");
-    await navigation.navigate("SignIn");
+    await navigation.navigate('SignIn');
+  };
+
+  const openLink = url => {
+    Linking.openURL(url);
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar backgroundColor="#000" barStyle="light-content" />
-      <View style={[styles.userInfoSection, { width: windowWidth * 0.9 }]}>
-        <View style={{ flexDirection: 'row', marginTop: 15 }}>
-          <Avatar.Image
-            source={require('../assets/Akdeniz_Universitesi.png')}
-            size={80}
-          />
-          <View style={{ marginLeft: 20 }}>
-            <Title style={[styles.title, { marginTop: 8, marginBottom: 5 }]}>email</Title>
-            <Caption style={styles.caption}>{userInfo.email}</Caption>
-          </View>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.profile}>
+          <Text style={styles.profileText}>Profile</Text>
         </View>
-      </View>
 
-      <View style={styles.userInfoSection}>
-        <View style={styles.row}>
-          <Icon name="map-marker-radius" color='#0063A9' size={22} />
-          <Text style={{ color: '#4F5458', marginLeft: 20, fontSize: 17 }}>Türkiye, Antalya</Text>
+        <View style={styles.containerCircular}>
+          <AnimatedCircularProgress
+            size={windowWidth * 0.45}
+            width={windowWidth * 0.04}
+            fill={progress}
+            tintColor="#DB6D2D"
+            backgroundColor="#f4f4f4"
+            rotation={0}
+            padding={10}
+            lineCap="round">
+            {() => (
+              <Text style={styles.progressInnerText}>Progress: {progress}%</Text>
+            )}
+          </AnimatedCircularProgress>
         </View>
-        <View style={styles.row}>
-          <Icon name="phone" color='#0063A9' size={22} />
-          <Text style={{ color: '#4F5458', marginLeft: 20, fontSize: 17 }}>+90 555 555 55 55</Text>
-        </View>
-        <View style={styles.row}>
-          <Icon name="email" color='#0063A9' size={22} />
-          <Text style={{ color: '#4F5458', marginLeft: 20, fontSize: 17 }}>ucar@gmail.com</Text>
-        </View>
-      </View>
 
-      <View style={styles.containerCircular}>
-        <AnimatedCircularProgress
-          size={180}
-          width={15}
-          fill={progress}
-          tintColor="#DB6D2D"
-          backgroundColor="#ccc"
-          rotation={0}
-          lineCap="round"
-        >
-          {() => <Text style={styles.menuItemText}>Progress: {progress}%</Text>}
-        </AnimatedCircularProgress>
-      </View>
+        <View style={styles.progressTexts}>
+          <View style={styles.progressTextsBox}>
+            <Text style={styles.progressTextsBoxTextNumber}>
+              {completedSteps}
+            </Text>
+            <Text>Completed</Text>
+          </View>
+          <View style={styles.progressTextsBox}>
+            <Text style={styles.progressTextsBoxTextNumber}>
+              {remainingSteps}
+            </Text>
+            <Text>Remaining</Text>
+          </View>
+        </View>
 
-      <View style={styles.menuWrapper}>
-        <TouchableRipple onPress={() => navigation.navigate('FavoriteQuestions')}>
-          <View style={styles.menuItem}>
-            <Icon name='heart' color='#0063A9' size={25} />
-            <Text style={styles.menuItemText}>Your Favorite Questions</Text>
-          </View>
-        </TouchableRipple>
-        <TouchableRipple onPress={() => { }}>
-          <View style={styles.menuItem}>
-            <Icon1 name='user' color='#0063A9' size={25} />
-            <Text style={styles.menuItemText}>Personal Information</Text>
-          </View>
-        </TouchableRipple>
-        <TouchableRipple onPress={() => { }}>
-          <View style={styles.menuItem}>
-            <Icon1 name='setting' color='#0063A9' size={25} />
-            <Text style={styles.menuItemText}>Settings</Text>
-          </View>
-        </TouchableRipple>
-        <TouchableRipple onPress={() => logout()}>
-          <View style={styles.menuItem}>
-            <Icon1 name='logout' color='#0063A9' size={25} />
-            <Text style={styles.menuItemText}>Logout</Text>
-          </View>
-        </TouchableRipple>
-      </View>
+        <View style={styles.menuWrapper}>
+          {links.length > 0 ? (
+            links
+              .sort((a, b) => a.sequence - b.sequence)
+              .map((item, index) => (
+                <TouchableRipple
+                  key={index}
+                  onPress={() => openLink(item.link)}>
+                  <View style={styles.menuItem}>
+                    <Icon1 name="link" color="#0063A9" size={windowWidth * 0.06} />
+                    <Text style={styles.menuItemText}>{item.text}</Text>
+                  </View>
+                </TouchableRipple>
+              ))
+          ) : (
+            <Text style={styles.errorText}>Linkler backendten çekilemiyor</Text>
+          )}
+          <TouchableRipple onPress={logout}>
+            <View style={[styles.menuItem, styles.logout]}>
+              <Icon1 name="logout" color="#0063A9" size={windowWidth * 0.06} />
+              <Text style={styles.menuItemText}>Logout</Text>
+            </View>
+          </TouchableRipple>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -142,36 +175,50 @@ export default function Profile({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'flex-center',
-    justifyContent: 'center',
-    backgroundColor: 'white'
+    backgroundColor: 'white',
   },
-  userInfoSection: {
-    paddingHorizontal: 30,
-    marginBottom: 25,
+  scrollContainer: {
+    paddingBottom: 30,
   },
-  title: {
-    fontSize: 25,
+  profile: {
+    marginTop: 10,
+    marginLeft: '10%',
+  },
+  profileText: {
+    fontSize: windowWidth * 0.08,
     fontWeight: 'bold',
     color: '#0063A9',
   },
-  caption: {
-    fontSize: 17,
-    lineHeight: 17,
-    fontWeight: '500',
-  },
-  row: {
-    flexDirection: 'row',
-    marginBottom: 10,
-  },
   containerCircular: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
+    flexDirection: 'column',
+    justifyContent: 'center',
     alignItems: 'center',
     paddingVertical: 20,
   },
+  progressTexts: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  progressTextsBox: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    margin: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 20,
+  },
+  progressTextsBoxTextNumber: {
+    fontSize: windowWidth * 0.05,
+    fontWeight: 'bold',
+    color: '#0063A9',
+  },
   menuWrapper: {
-    marginTop: 10,
+    flex: 1,
+    width: '100%',
   },
   menuItem: {
     flexDirection: 'row',
@@ -180,11 +227,34 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
     paddingHorizontal: 30,
   },
+  progressInnerText: {
+    color: '#0063A9',
+    fontWeight: '600',
+    fontSize: windowWidth * 0.04,
+  },
   menuItemText: {
     color: '#777777',
     marginLeft: 20,
     fontWeight: '600',
-    fontSize: 16,
-    lineHeight: 26
+    fontSize: windowWidth * 0.04,
+    lineHeight: windowHeight * 0.04,
+  },
+  errorText: {
+    color: 'red',
+    textAlign: 'center',
+    marginVertical: 20,
+    fontSize: windowWidth * 0.04,
+  },
+  logout: {
+    borderTopWidth: 2,
+    borderColor: '#ddd',
+  },
+  loading: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: '#777777',
+    fontWeight: '600',
+    fontSize: windowWidth * 0.04,
   },
 });
